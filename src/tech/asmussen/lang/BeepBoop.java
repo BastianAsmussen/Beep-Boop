48a996ac9fbe71cb82ec36d01888347b341a7c71
Bastian Asmussen <bastianasmussen@gmail.com>
2022-08-11 12:09:10 +0200

    Added source code.

--- src/tech/asmussen/lang/BeepBoop.java
+++ src/tech/asmussen/lang/BeepBoop.java
@@ -1,9 +1,35 @@
 package tech.asmussen.lang;
 
+import java.io.*;
+
 public class BeepBoop {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		System.out.println("Beep boop!");
+		File file;
+		
+		if (args.length == 0) {
+			
+			throw new IllegalArgumentException("Usage: java -jar BeepBoop.jar /path/to/file.bb!");
+			
+		} else {
+			
+			file = new File(args[0]);
+			
+			if (!file.getName().endsWith(".bb")) {
+				
+				throw new IllegalArgumentException("File must be a .bb file!");
+			}
+		}
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		
+		String line;
+		
+		while ((line = reader.readLine()) != null) {
+			
+			if (!line.equals(""))
+				
+				System.out.println(line);
+		}
 	}
 }
